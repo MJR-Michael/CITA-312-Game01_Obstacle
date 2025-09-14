d0630659782018b127411366231865b29d479177
Jandel <127899373+Jandel456@users.noreply.github.com>
2025-09-14 05:00:11 -0400

    Methods
    ~Refactored the mover script code so that the code responsible within movement is in a void function, and the code responsible for printing movement instructions is within its own void function.

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -6,14 +6,27 @@ public class Mover : MonoBehaviour
 
     void Start()
     {
-
+        PrintInstructions();
     }
 
     void Update()
+    {
+        MovePlayer();
+    }
+
+    void PrintInstructions()
+    {
+        Debug.Log("Welcome to the game!");
+        Debug.Log("Move using Arrow Keys or WASD");
+
+    }
+
+    void MovePlayer()
     {
         float xValue = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
         float yValue = 0f * Time.deltaTime;
         float zValue = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
         transform.Translate(xValue, yValue, zValue);
     }
+    
 }
