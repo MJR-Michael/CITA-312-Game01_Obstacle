764ffbf5d9e3e9565e320dbf54f505e10b120ca0
Jandel <127899373+Jandel456@users.noreply.github.com>
2025-09-14 06:36:30 -0400

    Improvement
    -Included sound effects when the player collides with an object.
    -Added the ability for the player to jump and float by pressing space while be affected by gravity once again.

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -24,7 +24,7 @@ public class Mover : MonoBehaviour
     void MovePlayer()
     {
         float xValue = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
-        float yValue = 0f * Time.deltaTime;
+        float yValue = Input.GetAxis("Jump") * moveSpeed * Time.deltaTime;
         float zValue = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
         transform.Translate(xValue, yValue, zValue);
     }
