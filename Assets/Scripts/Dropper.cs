66a385cf42a80b7af73eb4473bc0ef5ab76fdd76
Jandel <127899373+Jandel456@users.noreply.github.com>
2025-09-14 05:30:52 -0400

    Time.time
    -Added a dropping object
    -Added a new material for the dropper object
    -Created a Dropper script that prints the total time elapsed.

---
+++ Assets/Scripts/Dropper.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class Dropper : MonoBehaviour
+{
+    void Update()
+    {
+        float TotalTime = Time.time;
+        Debug.Log(TotalTime + " seconds have elapsed.");
+    }
+}
