d710deded5720e1c6bc60ac49d835c33e49f64f5
Jandel <127899373+Jandel456@users.noreply.github.com>
2025-09-14 05:45:29 -0400

    Caching
    -Added rigid body to the dropping object
    -Disabled Gravity and Mesh renderer at start for the dropping object
    -Updated Dropper script so it enables gravity after 2 seconds with TimeToWait variable

--- Assets/Scripts/Dropper.cs
+++ Assets/Scripts/Dropper.cs
@@ -3,6 +3,17 @@ using UnityEngine;
 public class Dropper : MonoBehaviour
 {
     [SerializeField] float TimeToWait = 2f;
+    MeshRenderer myMeshRender;
+    Rigidbody myRigidBody;
+
+    void Start()
+    {
+        myMeshRender = GetComponent<MeshRenderer>();
+        myMeshRender.enabled = false;
+
+        myRigidBody = GetComponent<Rigidbody>();
+        myRigidBody.useGravity = false;
+    }
     void Update()
     {
         float TotalTime = Time.time;
@@ -10,6 +21,9 @@ public class Dropper : MonoBehaviour
         if (TotalTime > TimeToWait)
         {
             Debug.Log("Lookout Below!");
+            myMeshRender.enabled = true;
+            myRigidBody.useGravity = true;
+
         }
     }
 }
